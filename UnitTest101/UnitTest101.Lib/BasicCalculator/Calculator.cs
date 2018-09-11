1cf07728e35ba77d423e5293888fad4cb5edffa1
Asif Rahman <asif0531@live.com>
2018-09-11 12:25:39 +0600

    initail commit with basic cal

---
+++ UnitTest101/UnitTest101.Lib/BasicCalculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest101.Lib.BasicCalculator
+{
+    public static class Calculator
+    {
+        public static double Add(double num1, double num2) => num1 + num2;
+        public static double Sub(double num1, double num2) => num1 - num2;
+        public static double Mul(double num1, double num2) => num1 * num2;
+        public static double Div(double num1, double num2) => num1 / num2;
+    }
+}
---
+++ UnitTest101/UnitTest101.Test/BasicCalculator/BasicCalculatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using UnitTest101.Lib;
+using UnitTest101.Lib.BasicCalculator;
+
+namespace UnitTest101.Test.BasicCalculator
+{
+    
+    public class BasicCalculatorTest
+    {
+        [InlineData(10, 12)]
+        [InlineData(5.5, 6.5)]
+        [InlineData(8, 9.7)]
+        [Theory]
+        public void AddMethod(double n1, double n2)
+        {
+            //assert
+            Assert.Equal(n1+n2, Calculator.Add(n1, n2));
+        }
+
+        [InlineData(10, 12)]
+        [InlineData(5.5, 6.5)]
+        [InlineData(8, 9.7)]
+        [Theory]
+        public void SubMethod(double n1, double n2)
+        {
+            //assert
+            Assert.Equal(n1 - n2, Calculator.Sub(n1, n2));
+        }
+
+        [InlineData(10, 12)]
+        [InlineData(5.5, 6.5)]
+        [InlineData(8, 9.7)]
+        [Theory]
+        public void MulMethod(double n1, double n2)
+        {
+            //assert
+            Assert.Equal(n1 * n2, Calculator.Mul(n1, n2));
+        }
+
+        [InlineData(10, 12)]
+        [InlineData(5.5, 6.5)]
+        [InlineData(8, 9.7)]
+        [Theory]
+        public void DivMethod(double n1, double n2)
+        {
+            //assert
+            Assert.Equal(n1 / n2, Calculator.Div(n1, n2));
+        }
+    }
+}
