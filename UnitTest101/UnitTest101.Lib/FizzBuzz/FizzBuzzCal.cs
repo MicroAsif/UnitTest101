4bb891c860f463a852dce6c6f4d2d33f92bb4ab3
Asif Rahman <asif0531@live.com>
2018-09-11 12:51:03 +0600

    fizzbuzz test added

---
+++ UnitTest101/UnitTest101.Lib/FizzBuzz/FizzBuzzCal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest101.Lib.FizzBuzz
+{
+    public static class FizzBuzzCal
+    {
+        public static string GetValue(int number)
+        {
+            string output = "";
+            if (number % 3 == 0)
+                output += "Fizz";
+            if (number % 5 == 0)
+                output += "Buzz";
+            if (string.IsNullOrEmpty(output))
+                output += number.ToString();
+            return output;
+        }
+    }
+}
---
+++ UnitTest101/UnitTest101.Test/FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTest101.Lib.FizzBuzz;
+using Xunit;
+
+namespace UnitTest101.Test.FizzBuzzTest
+{
+    public class FizzBuzzTest
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(6, "Fizz")]
+        [InlineData(10, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        public void FizzBuzz(int n, string m)
+        {
+            Assert.Equal(m, FizzBuzzCal.GetValue(n));
+        }
+    }
+}
